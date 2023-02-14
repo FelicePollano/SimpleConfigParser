0f9dec85487f57051f021fba6b53439b187ca85f
Felice Pollano <felice@felicepollano.com>
2023-02-14 16:54:29 +0100

    added config object

---
+++ src/GitConfigParser/Configuration.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Sprache;
+
+namespace GitConfigParser
+{
+    public class Configuration:Section
+    {
+        Section current;
+        Dictionary<string, Section> sections = new Dictionary<string, Section>();
+       
+        public void  Add(Stream s)
+        {
+            current = this;
+            using (var sr = new StreamReader(s))
+            {
+                var items = Parser.config.Parse(sr.ReadToEnd());
+            }
+        }
+    }
+}
